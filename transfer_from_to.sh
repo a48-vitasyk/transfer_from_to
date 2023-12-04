642b7c56f93d3603ae54bdc8c8e9d0d13c3cd2e7
a48-vitasyk <vitaly.stetsyuk@zomro.org>
2023-12-04 09:21:16 +0200

    md5sum

--- transfer_from_to.sh
+++ transfer_from_to.sh
@@ -20,6 +20,26 @@ attempt=0
 fixed_backoff_time=10 # время ожидания между попытками в секундах
 slack_webhook_url="SLACK_WEBHOOK_URL"
 
+function check_hash_before {
+    local path=$1
+    local user=$2
+    local host=$3
+    local password=$4
+
+    echo "$(sshpass -p $password ssh -o StrictHostKeyChecking=no $user@$host "md5sum $path" | awk '{ print $1 }')"
+}
+
+function check_hash_after {
+    local path=$1
+    local user=$2
+    local host=$3
+    local password=$4
+
+    echo "$(sshpass -p $password ssh -o StrictHostKeyChecking=no $user@$host "md5sum $path" | awk '{ print $1 }')"
+}
+
+
+
 # Функция отправки уведомления в Slack
 function send_slack_notification {
     message=$1
@@ -28,17 +48,28 @@ function send_slack_notification {
 
 while [ $attempt -lt $max_retries ]
 do
-  # Использование sshpass для автоматического ввода пароля
-  sshpass -p $server1_password ssh -o StrictHostKeyChecking=no $server1_user@$server1_host "sshpass -p $server2_password rsync -avzhP $source_path $server2_user@$server2_host:$destination_path" >> $log_file
-  if [ "$?" = "0" ] ; then
-    echo "$(date) - rsync completed normally" >> $log_file
-    send_slack_notification "rsync from $server1_host to $server2_host completed successfully"
-    exit
-  else
-    echo "$(date) - Rsync failure. Attempt $((attempt+1)) of $max_retries. Retrying in $fixed_backoff_time seconds..." >> $log_file
-    attempt=$((attempt+1))
-    sleep $fixed_backoff_time
-  fi
+    hash_before=$(check_hash_before "$source_path" $server1_user $server1_host $server1_password)
+
+    # Использование sshpass для автоматического ввода пароля...
+    # [команда rsync]
+
+    if [ "$?" = "0" ] ; then
+        hash_after=$(check_hash_after "$destination_path" $server2_user $server2_host $server2_password)
+
+        if [ "$hash_before" == "$hash_after" ]; then
+            echo "$(date) - rsync completed normally, hash match" >> $log_file
+            send_slack_notification "rsync from $server1_host to $server2_host completed successfully, hash match"
+            exit
+        else
+            echo "$(date) - Hash mismatch error after rsync" >> $log_file
+            send_slack_notification "Hash mismatch error after rsync from $server1_host to $server2_host"
+            exit 1
+        fi
+    else
+        echo "$(date) - Rsync failure. Attempt $((attempt+1)) of $max_retries. Retrying in $fixed_backoff_time seconds..." >> $log_file
+        attempt=$((attempt+1))
+        sleep $fixed_backoff_time
+    fi
 done
 
 error_message="$(date) - Failed to rsync after $max_retries attempts from $server1_host to $server2_host."
